b1c8124fd736c4f5626c76f6f857b0ecfb2f0f3c
JPcookie290 <julia.paier@yahoo.com>
2026-02-09 07:02:09 +0100

    initial file set up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { GameComponent } from './pages/game/game.component';
+import { EndComponent } from './pages/end/end.component';
 
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'end', component: EndComponent },
+];
